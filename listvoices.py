38ce69df0395e8a622876416395e2ee85f692c5e
pierretfie <maina350p@gmail.com>
2024-10-16 14:14:21 +0300

    initial commit

---
+++ listvoices.py
@@ -0,0 +1,8 @@
+import espeakng
+
+# Initialize the eSpeak NG engine
+espeak = espeakng.ESpeakNG()  # Create an instance
+
+
+# Say something
+espeak.say("Hello, this is a test using eSpeak NG.")
--- voice.py
+++ voice.py
@@ -1,12 +1,13 @@
+#windows
 import pyttsx3
-
-engine = pyttsx3.init(driverName='espeak')
-
-# List all available voices
-voices = engine.getProperty('voices')
-
-# Set a voice that you know should work
-engine.setProperty('voice', voices.id)  # Try the first available voice
-
-engine.say("Hello, this is a test.")
+from time import sleep
+engine = pyttsx3.init(driverName='sapi5')  # For Window
+voice = engine.getProperty('voices')
+engine.setProperty('voice', voice[1].id)
+engine.setProperty('rate', 170)
+engine.say("Hello, this is a test. Initializing sequence,please wait")
+sleep(2)
+engine.say('initialization complete')
 engine.runAndWait()
+sleep(3)
+
