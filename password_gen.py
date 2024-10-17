3d38a4c07454c0da98f5ebd1aeb4052bfd8b4d03
pierretfie <maina350p@gmail.com>
2024-10-17 23:56:05 +0300

    initial commit

--- password_gen.py
+++ password_gen.py
@@ -1,29 +1,43 @@
-
+import pyttsx3
 import random
 import string
 import qrcode
 from os import path
 from colorama import Fore, Style
 
-
 print(Fore.RED+ '****************scripted by pierre-tfie********************')
-def password(len):
+def password():
+    engine = pyttsx3.init(driverName='sapi5')
+    voice = engine.getProperty('voices')
+    engine.setProperty('voice',  voice[1].id)
+    engine.setProperty('rate', 150)
+    engine.say("hello, I'm Brain, you have requested a new password. What should be the password length?")
+    engine.runAndWait()
+    len=input(Fore.RESET+'ENTER LENGTH OF PASS TO GENERATE:\n')
+    
     while True:
         if len[0] not in string.digits:
-            print(Style.BRIGHT + 'ERROR!!ENTER A NUMBER:\n')
-            len=input(Fore.RESET+'ENTER LENGTH OF PASS TO GENERATE:\n')
+            print(f'{Style.BRIGHT}{Fore.RED}ERROR!!ENTER A NUMBER:\n')
+
+            engine.say('invalid input, reenter password length')
+            engine.runAndWait()
+            len=input(f'{Style.RESET_ALL}{Fore.RESET}ENTER LENGTH OF PASS TO GENERATE:\n')
         else:
+            engine.say('add the password usage')
+            engine.runAndWait()
             break
+   
     filename = input('password use:\n')
+    
     l = string.ascii_lowercase
     u = string.ascii_uppercase
     c = string.punctuation
     d = string.digits
     combination = l + u + c + d
     output = ''
+    
     for i in range(int(len)):
         output += random.choice(combination)
-    print(f'{Fore.GREEN}password is >>>>>>>>>>>>\n\n\n{output}{Style.RESET_ALL}')
     data = output
     filename = filename+'.png'
     qr = qrcode.QRCode(
@@ -31,13 +45,14 @@ def password(len):
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size=5,
     border=8,
-)
-
+    )
     qr.add_data(data)
     qr.make(fit=True)
     img = qr.make_image(fill_color='black', back_color='aqua' )
     dir = path.expanduser('~/Documents')
     img.save(path.join(dir, filename))
-
-
-password(len=input(Fore.RESET+'ENTER LENGTH OF PASS TO GENERATE:\n'))
+    engine.say(f'your {len} length password has been generated and saved in your Documents, nice time securing your accounts')
+    engine.runAndWait()
+    
+if __name__ == '__main__':
+    password()
--- voice.py
+++ voice.py
@@ -1,13 +1,9 @@
 #windows
 import pyttsx3
 from time import sleep
-engine = pyttsx3.init(driverName='sapi5')  # For Window
+engine = pyttsx3.init(driverName='sapi5')
 voice = engine.getProperty('voices')
-engine.setProperty('voice', voice[1].id)
+engine.setProperty('voice',  voice[1].id)
 engine.setProperty('rate', 170)
-engine.say("Hello, this is a test. Initializing sequence,please wait")
-sleep(2)
-engine.say('initialization complete')
+engine.say('hello')
 engine.runAndWait()
-sleep(3)
-
