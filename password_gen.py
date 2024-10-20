8f3d6a1becd68ee2a5e5f1b4bf121c5247954df3
pierretfie <maina350p@gmail.com>
2024-10-20 15:35:44 +0300

    initial commit

--- password_gen.py
+++ password_gen.py
@@ -41,10 +41,32 @@ def password():
     return output, filename, len
 
 
-    
+#store  the password in a QR code
+def create_qrcode():
+        
+    engine = pyttsx3.init(driverName='sapi5')
+    voice = engine.getProperty('voices')
+    engine.setProperty('voice',  voice[1].id)
+    engine.setProperty('rate', 150)
+    data = gen_output[0]
+    filename =  gen_output[1]
+    len = gen_output[2]
+    filename = filename+'.png'
+    qr = qrcode.QRCode(
+    version=5,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=5,
+    border=8,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color='black', back_color='aqua' )
+    dir = path.expanduser('~/Documents')
+    img.save(path.join(dir, filename))
+    engine.say(f'your {len} length password has been generated and saved in your Documents, nice time securing your accounts')
+    engine.runAndWait()
     
 if __name__ == '__main__':
     gen_output = password()
-def create_qrcode():
-    
-create_qrcode()
+    create_qrcode()
+ 
