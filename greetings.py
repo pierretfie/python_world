37274f6784db842dc0b2a217c628eb6f48ae281f
pierretfie <maina350p@gmail.com>
2024-10-21 21:47:14 +0300

    initial commit

--- greetings.py
+++ greetings.py
@@ -1,13 +1,13 @@
 #a model for answering greetings
 from subprocess import run
+import random
 import nltk
 from nltk.tokenize import word_tokenize
-#nltk.download('punkt')
-#nltk.download('punkt_tab')
+
 #first we need a dataset for possible greetings(we can make it formal)
 def greet_response (user_input):
     #we will use a dictionary to map the user's input to a response
-    greetings = ["hello", "hi", "hey", "greetings", "what's up"]
+    greetings = ["hello", "hi", "hey", "greetings", "what's up", "hola"]
     responses = [
     "Hello! How can I help you?",
     "Hi there! What do you need?",
@@ -19,15 +19,32 @@ def greet_response (user_input):
     # to different greetings with a single word know to the model eg 'hi'
     user_input = user_input.lower()
     #we are breaking down the user iput
-    tokens = word_tokenize(user_input)
+    try:
+
+        tokens = word_tokenize(user_input)
+    except Exception as e:
+        nltk.download('punkt')
+        nltk.download('punkt_tab')
+    else:
+        pass
+
     #filter response
     for token in tokens:
         if token in greetings:
             #return a response which is at the same position as the greeting
-            return responses[greetings.index(token)]
+            return random.choice(responses)
     return  "Sorry, I didn't understand that."
 
 user_input = input('You:')
 response = greet_response(user_input)
 print('Bot:', response)
-run(['espeak',response])
+try:
+    run(['espeak',response])
+except Exception as e:
+    import pyttsx3
+    engine = pyttsx3.init(driverName='sapi5')
+    voice = engine.getProperty('voices')
+    engine.setProperty('voice',voice[0].id )
+    engine.setProperty('rate', 190)
+    engine.say(response)
+    engine.runAndWait()
