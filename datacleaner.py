927559514ba32ead651b8cac995561083ee942b1
maina peter <134748966+pierretfie@users.noreply.github.com>
2024-10-24 09:19:07 +0300

    Update datacleaner.py

--- datacleaner.py
+++ datacleaner.py
@@ -1,11 +1,1 @@
-#this code adds words into a list
-def add_to_list():
-    list = []
-    words = input('paste your words:\n')
-    for word in words:
-        word = word.strip()
-        list.append(word)
-        return list
-    
-output = add_to_list()
-print(output)
+
