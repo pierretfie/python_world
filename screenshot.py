514cda7cdd4e77f9eb4de3667a3fa42c456cd213
pierretfie <maina350p@gmail.com>
2024-12-01 22:13:06 +0300

    commit

--- screenshot.py
+++ screenshot.py
@@ -4,15 +4,15 @@ def screensht():
     add = 0
     dir = '~/Pictures'
     screenshot = ImageGrab.grab()
-   
-    name = f'screenshot{add}.png'
+    try:
+        name = f'screenshot{add}.png'
 
-    screenshot.save(os.path.join(os.path.expanduser(dir), name))  
-    if name in os.listdir(os.path.expanduser(dir)):
+        screenshot.save(os.path.join(os.path.expanduser(dir), name))  
+    except FileExistsError:
       
         
          name = f'screenshot{add+1}.png'
          screenshot.save(os.path.join(os.path.expanduser(dir), name)) 
     return name
-if __name__ == '__main__':
-    screensht()
+if __name__ == '__main__'
+screensht()
