c2ac2485d7afc2792c4f0524bab9db9ae8a0e972
pierretfie <maina350p@gmail.com>
2024-12-04 10:12:32 +0300

    commit

--- authentication_input.py
+++ authentication_input.py
@@ -1,8 +1,22 @@
 import getpass
 
-def login():
-    auth_user = getpass.getuser('Enter username:')
+def user_input():
+    auth_user = input('Enter username:')
     autha_pass = getpass.getpass('Enter password: ')
+    #you can add confirmation and password reference blocks
+    return auth_user, autha_pass
+def confirm():
+    details = user_input()
+    passwd = 'password'
+    if details[0] == 'peter':
+        if passwd == details[1]:
+            print('Authentication successful')
+        else:
+            print('Authentication failed')
+        exit()
+    else:
+        print('Unknown User')
+confirm()
 
-if __name__ == '__main__':
-    login()
+
+
