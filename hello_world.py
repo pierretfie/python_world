cada583e245552c2ebc1f0b4886630ffa30cf217
maina peter <134748966+pierretfie@users.noreply.github.com>
2024-12-09 12:22:54 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -5,3 +5,8 @@ s = '*'
 for i in word
     index = word.index(i)
     print (f'{i} {s*index}')
+
+if 'e' in word:
+    print("a letter 'e' found in the word")
+else:
+    print("demanded letter not found")
