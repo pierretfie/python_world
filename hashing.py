1de2fb7b7db77803c7bf10e357a9950bea93ea8b
maina peter <134748966+pierretfie@users.noreply.github.com>
2024-12-12 14:44:22 -0500

    Update hashing.py

--- hashing.py
+++ hashing.py
@@ -4,16 +4,19 @@ data = password()
 print(data)
 message = data[0]
 
+def hashdata ():
 
-#create hash object
-hash_object = hashlib.sha256()
+    #create hash object
+    hash_object = hashlib.sha256()
 
-#convert the input data to bytes and update hash object
-hash_object.update(message.encode('utf-8'))
+    #convert the input data to bytes and update hash object
+    hash_object.update(message.encode('utf-8'))
 
-# get the hexadecimal representation of the hash value
-hashed_data = hash_object.hexdigest()
-print(hashed_data)
+    # get the hexadecimal representation of the hash value
+    hashed_data = hash_object.hexdigest()
+    print(hashed_data)
+if __name__ == '__main__'
+   
 
 
 
