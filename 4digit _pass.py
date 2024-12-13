783700b54456efb3eca9577ad5a4edd3be025da4
maina peter <134748966+pierretfie@users.noreply.github.com>
2024-12-13 14:01:01 -0500

    Update 4digit _pass.py

--- 4digit _pass.py
+++ 4digit _pass.py
@@ -5,4 +5,7 @@ def 4digits():
     for num in range(10000):  # Numbers from 0000 to 9999
         with open('digits.txt','a') as file:
             file.write(f'{num:04d}\n')
+if __name__ = '__main__':
+    4digits()
+
 
