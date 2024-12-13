7fcb03870a90a2d404006e910d7812dfb6181534
maina peter <134748966+pierretfie@users.noreply.github.com>
2024-12-13 13:27:28 -0500

    Update 4digit _pass.py

--- 4digit _pass.py
+++ 4digit _pass.py
@@ -1,5 +1,8 @@
 # Generate all possible 4-digit combinations
-for num in range(10000):  # Numbers from 0000 to 9999
-    with open('digits.txt','a') as file:
-        file.write(f'{num:04d}\n')
+
+def 4digits():
+
+    for num in range(10000):  # Numbers from 0000 to 9999
+        with open('digits.txt','a') as file:
+            file.write(f'{num:04d}\n')
 
