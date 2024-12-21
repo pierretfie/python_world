1698ad188b786de786be9fceb5fc22b84c662965
pierretfie <maina350p@gmail.com>
2024-12-21 08:32:08 +0300

    locking in

--- AES_ENcryption.py
+++ AES_ENcryption.py
@@ -1,13 +1,22 @@
 from cryptography.fernet import Fernet
 
-# Generate a key
-key = Fernet.generate_key()
-cipher = Fernet(key)
 
-# Encrypt data
-plaintext = b"Hello, World!"
-ciphertext = cipher.encrypt(plaintext)
+def aescrypt():
+    # Generate a key
+    key = Fernet.generate_key()
+    cipher = Fernet(key)
 
-# Decrypt data
-decrypted_text = cipher.decrypt(ciphertext)
-print(decrypted_text.decode())
+    # Encrypt data
+    plaintext = input(b"Enter data to encrypt: ")
+    ciphertext = cipher.encrypt(plaintext)
+    print(ciphertext)
+    return ciphertext, cipher
+
+def aesdecrypt():
+    cipher = aescrypt[0]
+    ciphertext = aescrypt[1]
+    # Decrypt data
+    decrypted_text = cipher.decrypt(ciphertext)
+    print(decrypted_text.decode())
+if __name__ == '__main__':
+    aesdecrypt()
