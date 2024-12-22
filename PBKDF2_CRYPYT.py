74f08bc8fd957c46e8a5310c123628a5d8368785
pierretfie <maina350p@gmail.com>
2024-12-22 10:26:39 +0300

    commit

--- PBKDF2_CRYPYT.py
+++ PBKDF2_CRYPYT.py
@@ -1,9 +1,14 @@
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-import os, hashlib
+from cryptography.hazmat.primitives import hashes
+import os
 
 
-def pbkd(data):
-    #creating a salt
+#PBKDF2 is not reversible
+def pbkd():
+    # Prompt for data to encrypt
+    data = input('ENTER Data to encrypt: ')
+    
+    # Creating a salt
     salt = os.urandom(16)
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
@@ -11,6 +16,11 @@ def pbkd(data):
         salt=salt,
         iterations=100000,
     )
-    data = 
-    key = kdf.derive(b"mypassword")
-pbkd(data = input('ENTER Data to encrypt: '))
+    data = data.encode('utf-8')  # Ensure data is in bytes
+    key = kdf.derive(data)  # Derive the key
+    #print(key)
+    return key  # Return the derived key
+
+if __name__ == '__main__':
+    derived_key = pbkd()
+    print("Derived Key:", derived_key.hex())  # Print the derived key in hexadecimal format
