825cd535ab0e84acc2d983a105f0604b4ae18ed3
pierretfie <maina350p@gmail.com>
2024-12-22 10:28:48 +0300

    commit

--- AES_ENcryption.py
+++ AES_ENcryption.py
@@ -13,13 +13,13 @@ def aescrypt():
     ciphertext = cipher.encrypt(plaintext_bytes)
     print(ciphertext)
     return ciphertext, key
-def aesdecrypt():
+'''def aesdecrypt():
     key = data[1]
     cipher = Fernet(key)
     ciphertext = data[0]
     # Decrypt data
-    #decrypted_text = cipher.decrypt(ciphertext)
-    #print(decrypted_text.decode())
+    decrypted_text = cipher.decrypt(ciphertext)
+    print(decrypted_text.decode())'''
 if __name__ == '__main__':
     data = aescrypt()
     aesdecrypt()
